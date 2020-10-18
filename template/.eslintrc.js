e62829b82842212bc0f68e26ba3cb5c8298aceff
Matus Hruz <hruzmatus@gmail.com>
2020-10-18 20:42:02 +0200

    v.1.0.13

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -18,17 +18,16 @@ module.exports = {
   extends: [
     "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    // "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
     "react-app",
   ],
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    "@typescript-eslint/ban-ts-comment": [
-      prodErrorOthersWarn,
-      { "ts-ignore": "allow-with-description" },
-    ],
-    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
+    "react/display-name": "off", // TODO: inspect why its not working well with functional components
+    "react/prop-types": "off", // TODO: inspect why its nto working well with TS
+    // "@typescript-eslint/explicit-function-return-type": "off",
     "jsx-quotes": ["error", "prefer-double"],
     "no-unused-vars": prodErrorOthersWarn,
     "no-debugger": prodErrorOthersWarn,
