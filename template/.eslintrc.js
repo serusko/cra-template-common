88c2f50070e1acd3a3da7a9adb725f7514f52796
Matus Hruz <hruzmatus@gmail.com>
2021-02-07 16:32:16 +0100

    fix eslint

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,38 +1,67 @@
 const prodErrorOthersWarn =
   process.env.NODE_ENV === "production" ? "error" : "warn";
+const prodNoErr = process.env.NODE_ENV === "production" ? "off" : "warn";
 
 module.exports = {
-  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  env: {
+    node: true,
+  },
+  extends: [
+    "react-app", // Uses the recommended rules Create React App
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from @typescript-eslint/eslint-plugin
+    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    "plugin:prettier/recommended", // Should be last in the list. Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    "plugin:jsx-a11y/recommended",
+  ],
+  overrides: [
+    {
+      files: ["./**/*.ts?(x)"],
+      rules: {
+        "@typescript-eslint/ban-ts-comment": [
+          prodErrorOthersWarn,
+          { "ts-ignore": "allow-with-description" },
+        ],
+        "@typescript-eslint/ban-ts-ignore": "off",
+        "@typescript-eslint/no-unused-vars": [
+          prodErrorOthersWarn,
+          { argsIgnorePattern: "^_" },
+        ],
+        "@typescript-eslint/no-use-before-define": "off",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-    sourceType: "module", // Allows for the use of imports
     ecmaFeatures: {
       jsx: true, // Allows for the parsing of JSX
     },
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
   },
-  settings: {
-    react: {
-      version: "detect", // Tells eslint-plugin-react to automatically detect the version of React to use
-    },
-  },
-  extends: [
-    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-    // "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
-    "react-app",
-  ],
+  plugins: ["import", "jsx-a11y", "eslint-plugin-sort-keys-fix"],
   rules: {
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    "@typescript-eslint/ban-ts-comment": "off",
-    "react/display-name": "off", // TODO: inspect why its not working well with functional components
-    "react/prop-types": "off", // TODO: inspect why its nto working well with TS
-    // "@typescript-eslint/explicit-function-return-type": "off",
-    "jsx-quotes": ["error", "prefer-double"],
-    "no-unused-vars": prodErrorOthersWarn,
-    "no-debugger": prodErrorOthersWarn,
+    "jsx-a11y/click-events-have-key-events": prodErrorOthersWarn,
+    "jsx-a11y/no-noninteractive-element-interactions": prodErrorOthersWarn,
     "no-console": prodErrorOthersWarn,
+    "no-debugger": prodErrorOthersWarn,
+    "no-restricted-imports": [
+      prodErrorOthersWarn,
+      {
+        paths: [
+          // TODO: use custom rules
+          {
+            message: "Please import from providers/theme.",
+            name: "styled-components",
+          },
+        ],
+        patterns: [],
+      },
+    ],
+    "no-unused-vars": "off",
+    "no-use-before-define": "off",
+    "prefer-const": "warn",
     "react/jsx-handler-names": "warn",
-    "prefer-const": ["warn"],
+    "react/jsx-sort-props": [prodErrorOthersWarn, { callbacksLast: true }],
+    "sort-keys-fix/sort-keys-fix": prodNoErr,
   },
 };
